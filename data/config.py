f9338d18688cc95625feebcc4f51b89396d9d8b3
operose123 <nfelix4@hawkmail.hccfl.edu>
2024-11-10 19:18:07 +0000

    Formatted code for better readability, added car_db.txt for authorized vehicle inventory, added functionality to add a new vehicle to the list.

---
+++ data/config.py
@@ -0,0 +1,41 @@
+def openDB():
+  return ('data/car_db.txt')
+
+ADDCAR = (
+"\nPlease Enter the full Vehicle name you would like to add: "
+)
+
+AUTHCARS = (
+"\nThe AutoCountry sales manager has authorized the purchase \
+and selling of the following vehicles:\n"
+)
+
+ENTERCAR = "\nPlease enter the full vehicle name: "
+
+GOODBYE = (
+"\nThank you for using the AutoCountry Vehicle Finder, \
+good-bye! Press Ctrl+C to end program.\n"
+)
+
+INVALIDINPUT = "\nInvalid input.\n"
+
+ISAUTHORIZED = " is an authorized vehicle"
+
+MAINMENU = """
+********************************
+AutoCountry Vehicle Finder v0.3
+********************************
+Please Enter the following number below from the following menu:
+
+1. PRINT all Authorized Vehicles
+2. SEARCH for Authorized Vehicle
+3. ADD Authorized Vehicle
+4. Exit
+"""
+
+NOTAUTHORIZED = (
+" is NOT an authorized vehicle, if you received this in error \
+please check the spelling and try again"
+)
+
+CHOOSEOPTION = "Please enter a number to select an option: "
--- main.py
+++ main.py
@@ -1,47 +1,58 @@
-allowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
-optionSelection = 0
-vehicleToSearch = ""
-vehicleIndexNumber = 0
-authorizedVehicleMessage = "\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:\n" 
-optionSelectionPrompt = "Please enter 1, 2 or 3 to select an option: "
-vehicleSearchPrompt = "\nPlease enter the full vehicle name: "
-authorizedVehicleFound = " is an authorized vehicle"
-authorizedVehicleNotFound = " is not an authorized vehicle, if you received this in error please check the spelling and try again"
-inputErrorMessage = "\nInvalid input.\n"
-exitMessage = "\nThank you for using the AutoCountry Vehicle Finder, good-bye! Press Ctrl+C to end program.\n"
-mainMenu = """********************************
-AutoCountry Vehicle Finder v0.1
-********************************
-Please Enter the following number below from the following menu:
+import sys
 
-1. PRINT all Authorized Vehicles
-2. SEARCH for Authorized Vehicle
-3. Exit
-"""  
+sys.path.append('data')
+
+from config import (
+  ADDCAR,
+  CHOOSEOPTION,
+  ENTERCAR,
+  GOODBYE,
+  INVALIDINPUT,
+  ISAUTHORIZED,
+  MAINMENU,
+  NOTAUTHORIZED,
+  openDB,
+)
+
+carIndexVal = 0
+menuOption = 0
+newCar = ""
+searchFor = ""
+
 while True:
-  print(mainMenu)
+  print(MAINMENU)
   try:
-    optionSelection = int(input(f"{optionSelectionPrompt}"))
-  except:
-    print(inputErrorMessage)
+    menuOption = int(input(f"{CHOOSEOPTION}"))
+  except Exception:
+    print(INVALIDINPUT)
     continue
-  if optionSelection == 1:
-    print(authorizedVehicleMessage)
-    for vehicle in allowedVehiclesList:
-      print(vehicle + "\n")
-  elif optionSelection == 2:
-    vehicleToSearch = input(vehicleSearchPrompt)
-    try:
-      vehicleIndexNumber = allowedVehiclesList.index(vehicleToSearch)
-      print(f"\n{vehicleToSearch}{authorizedVehicleFound}\n")
-    except ValueError:
-      print(f"\n{vehicleToSearch}{authorizedVehicleNotFound}\n")
-  elif optionSelection == 3:
-    print(exitMessage)
-    try:
-      while True:
-        pass
-    except KeyboardInterrupt:
-      break
+    
+  if menuOption == 1:
+    with open(openDB(), 'r') as carDB:
+      print("\n" + carDB.read())
+    
+  elif menuOption == 2:
+    with open(openDB(), 'r') as carDB:
+      carIndexVal = [line.strip() for line in carDB]
+      searchFor = input(ENTERCAR).strip()
+      if searchFor in carIndexVal:
+        print("\n" + f"{searchFor}{ISAUTHORIZED}")
+      else:
+        print("\n" + f"{searchFor}{NOTAUTHORIZED}")
+      
+  elif menuOption == 3:
+    newCar = input(ADDCAR)
+    with open(openDB(), 'a') as carDB:
+      carDB.write(newCar + '\n')
+    print(f"\nYou have added '{newCar}' as an authorized vehicle")
+
+  elif menuOption == 4:
+   print(GOODBYE)
+   try:
+    while True:
+      pass
+   except KeyboardInterrupt:
+    break
+  
   else:
-    print(inputErrorMessage)
+    print(INVALIDINPUT)
